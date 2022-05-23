158ad37f266c41bac455420013733685a9f021a9
ioanniskermanidis <gianniskermanidis@gmail.com>
2022-05-24 00:34:49 +0300

    Formatted output

--- askisi2.c
+++ askisi2.c
@@ -54,7 +54,7 @@ int main()
 	while(1)
 	{
 	printTitle();
-	printf(" 1.Add New Contact\n 2.Search Contact\n 3.Update Contact\n 4.Delete Contact\n 5.List Contacts\n 6.Exit\n ");
+	printf(" 1.Add New Contact\n 2.Search Contact\n 3.Update Contact\n 4.Delete Contact\n 5.List Contacts\n 6.Exit\n");
 	char choice;
 	choice=getch();
 	switch(choice){
@@ -171,7 +171,7 @@ contact* addContact(contact* contacts, int* ptr_contactCounter){
 	scanf("%s", contacts[contactCounter - 1].email);
 	printf(" Phone: ");
 	scanf("%s", contacts[contactCounter - 1].phone);
-	printf("\nNew entry added!\n");
+	printf("\n New entry added!\n");
 	return contacts;
 }
 
@@ -196,18 +196,18 @@ void searchContact(contact* contacts, int contactCounter)
 	printTitle();
 	printf("-->Search Contact\n\n");
 	char name[100];
-	printf("Enter contact name:");
+	printf(" Enter contact name:");
 	scanf("%s", name);
 	int i=0;
 	for ( i=0; i<contactCounter; i++){
 		if(strcmp(name,contacts[i].name)==0 && contacts[i].deleted==0)
 		{
-			printf("Record found\n");
+			printf(" Record found\n");
 			printContact(contacts[i]);
 			return;
 		}
 	}
-	printf("Record not found\n");
+	printf(" Record not found\n");
 }
 
 void updateContact(contact* contacts, int contactCounter)
@@ -229,7 +229,7 @@ void updateContact(contact* contacts, int contactCounter)
 			scanf("%s", contacts[i].email);
 			printf("Enter new phone nubmer:");
 			scanf("%s", contacts[i].phone);
-			printf("\nSelected entry updated!");
+			printf("\n Selected entry updated!");
 			return;
 		}
 	}
@@ -240,7 +240,7 @@ void deleteContact(contact* contacts, int contactCounter)
 	printTitle();
 	printf("-->Delete Contact\n\n");
 	char name[100];
-	printf("Enter contact name:");
+	printf(" Enter contact name:");
 	scanf("%s", name);
 	int i=0;
 	for ( i=0; i<contactCounter; i++)
@@ -248,7 +248,7 @@ void deleteContact(contact* contacts, int contactCounter)
 		if(strcmp(name,contacts[i].name)==0 && contacts[i].deleted==0)
 		{
 			contacts[i].deleted=1;
-			printf("Record deleted succesfully");
+			printf(" Record deleted succesfully");
 			return;
 		}
 	}
